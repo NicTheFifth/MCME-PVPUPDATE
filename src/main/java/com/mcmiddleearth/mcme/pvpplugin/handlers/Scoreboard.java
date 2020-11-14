cc08f2127305d7e5294aec0cf74518e0422ddca3
John <klironomos.john@gmail.com>
2020-11-14 14:37:10 +0200

    More organising

--- src/main/java/com/mcmiddleearth/mcme/pvpplugin/handlers/Scoreboard.java
+++ src/main/java/com/mcmiddleearth/mcme/pvpplugin/handlers/Scoreboard.java
@@ -2,4 +2,6 @@ package com.mcmiddleearth.mcme.pvpplugin.handlers;
 
 public class Scoreboard {
     //scoreboard after the end of the game
+    //counts kills, deaths, K/D, maybe objectives in specific gamemodes
+    //maybe an mvp system???,maybe not
 }
