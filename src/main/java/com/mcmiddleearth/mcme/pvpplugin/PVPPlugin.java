eb32cb41fb179afcf2d8b0b1cb50da1aecb47ff5
NicTheFifth <cinnic1234@gmail.com>
2021-07-09 21:46:38 +0200

    Start of enabling/loading

--- src/main/java/com/mcmiddleearth/mcme/pvpplugin/PVPPlugin.java
+++ src/main/java/com/mcmiddleearth/mcme/pvpplugin/PVPPlugin.java
@@ -1,6 +1,8 @@
 package com.mcmiddleearth.mcme.pvpplugin;
 
+import java.io.File;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -14,6 +16,8 @@ import lombok.Getter;
 import lombok.Setter;
 import net.md_5.bungee.api.CommandSender;
 import net.md_5.bungee.api.chat.ComponentBuilder;
+import org.bukkit.Bukkit;
+import org.bukkit.WorldCreator;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -34,13 +38,53 @@ public class PVPPlugin extends JavaPlugin {
     @Getter@Setter
     GamemodeRunner activeGame;
 
-    @Override
-    public void onEnable() {
-        loadConfig();
-        setup();
+    private File mapDirectory;
 
+    @Override
+    public void onLoad(){
+        this.saveDefaultConfig();
+        this.reloadConfig();
+        if(this.getConfig().contains("noHunger")){
+            noHunger.addAll(this.getConfig().getStringList("noHunger"));
+        }
+        CLog.println(getDataFolder().getPath());
+        if (!getDataFolder().exists()){
+            getDataFolder().mkdir();
+        }
+        mapDirectory = new File(getDataFolder() + System.getProperty("file.separator") + "maps");
+        if (!mapDirectory.exists()){
+            mapDirectory.mkdir();
+        }
+        this.statDirectory = new File(getDataFolder() + System.getProperty("file.separator") + "stats");
+        if (!statDirectory.exists()){
+            statDirectory.mkdir();
+        }
+        this.getServer().getMessenger().registerOutgoingPluginChannel(this, "BungeeCord");
+        if(getServer().getPluginManager().isPluginEnabled("PlaceholderAPI")) {
+            //PlaceholderAPI.registerPlaceholderExpansion("mcmePvP", new com.mcmiddleearth.mcme.pvp.Handlers.ChatHandler());
+            new com.mcmiddleearth.mcme.pvp.Handlers.ChatHandler().register();
+        } else {
+            Logger.getGlobal().warning("PlaceholderAPI not enabled");
+        }
+        HashMap<String, Object> maps = new HashMap<>();
+        try{
+            maps = DBmanager.loadAllObj(Map.class, this.mapDirectory);
+        }
+        catch(Exception ex){
+        }
+        if(maps == null){
+            maps = new HashMap<>();
+        }
+        //Spawn = new Location(Bukkit.getWorld("world"), 344.47, 39, 521.58, 0.3F, -24.15F);
 
         Logger.getLogger("PVPPlugin").log(Level.INFO, "PVPPlugin loaded correctly");
+    }
+
+    @Override
+    public void onEnable() {
+        setup();
+
+        Logger.getLogger("PVPPlugin").log(Level.INFO, "PVPPlugin enabled correctly");
     }
 
     private void setup() {
@@ -51,9 +95,6 @@ public class PVPPlugin extends JavaPlugin {
     @Override
     public void onDisable() {
 
-    }
-
-    private void loadConfig() {
     }
 
     public static void sendInfo(CommandSender recipient, ComponentBuilder message) {
